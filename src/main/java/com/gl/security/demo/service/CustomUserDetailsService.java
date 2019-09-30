d069324b35b74413c888593cc79d8cccf6fef74d
Pravin Asati <asati.pravin18@gmail.com>
2019-09-30 16:16:17 +0530

    Added impl.

---
+++ src/main/java/com/gl/security/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.gl.security.demo.service;
+
+import com.gl.security.demo.model.CustomUserDetails;
+import com.gl.security.demo.repository.UsersRepository;
+import com.gl.security.demo.model.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Users> optionalUsers = usersRepository.findByName(username);
+
+        optionalUsers
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return optionalUsers
+                .map(CustomUserDetails::new).get();
+    }
+}
